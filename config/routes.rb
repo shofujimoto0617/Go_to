54e75b31bd4dc9ed267825aa61f4ade87c9344cb
vagrant <vagrant@localhost.localdomain>
2020-09-07 13:41:07 +0000

    add:show to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :edit]
 end
