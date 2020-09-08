331ecbf5d86ba9b5379881ae71137184421c29b7
vagrant <vagrant@localhost.localdomain>
2020-09-08 05:49:50 +0000

    add:user_edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # refielのためのattachment
+  attachment :image, destroy: false
+
+  # バリデーション/エラーにする条件
+  validates :user_name, presence: true, length: {maximum: 10, minimum: 2}
+  validates :account_name, length: {maximum: 20}
+  validates :introduction, length: {maximum: 50}
 end
