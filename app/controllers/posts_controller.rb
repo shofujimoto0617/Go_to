6c628ce055dfd9e539d7e747e0f59a116e921738
vagrant <vagrant@localhost.localdomain>
2020-09-09 13:44:44 +0000

    Fix:start_date,finish_date,post_image of post_new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,24 @@ class PostsController < ApplicationController
   	# postクラスに関連づいたpost_imageクラスのインスタンス
   	@post.post_images.build
   end
+
+  def index
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	@post.user_id = current_user.id
+  	# start_dateをdate型に変換
+  	@post.start_date = params[:start_date].to_date
+  	# finish_dateをdate型に変換
+  	@post.finish_date = params[:finish_date].to_date
+  	@post.save
+  	redirect_to posts_path
+  end
+
+
+  private
+  def post_params
+  	params.require(:post).permit(:country, :place, :body, :start_date, :finish_date, :price, post_images_images: [])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_100247) do
+ActiveRecord::Schema.define(version: 2020_09_09_125915) do
 
   create_table "post_images", force: :cascade do |t|
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "cuntry", null: false
+    t.integer "country", null: false
     t.string "place", default: "", null: false
     t.text "body"
     t.integer "price"
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_08_100247) do
     t.datetime "updated_at", null: false
     t.date "start_date"
     t.date "finish_date"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
