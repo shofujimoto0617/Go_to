d72a467b550a111a95cd9171b9456b709261f3c1
vagrant <vagrant@localhost.localdomain>
2020-09-10 05:41:20 +0000

    add:post_indaex

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9877c3c46168d1d81090e5c139c434ea29da3d2fa4fc55151456125dd4ed4d2e24907393847f962ab5621822bbc1136688ba850b9e9a0798ffd5c9cde910d31b'
