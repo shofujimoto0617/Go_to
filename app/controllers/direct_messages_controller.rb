512e78ec17a8f17869e1ad656ae2619619149326
vagrant <vagrant@localhost.localdomain>
2020-09-21 16:12:10 +0000

    update:room_show to DM

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -3,7 +3,8 @@ class DirectMessagesController < ApplicationController
   	@direct_message = DirectMessage.new(direct_message_params)
   	@direct_message.user_id = current_user.id
   	if @direct_message.save
-  	  redirect_to room_path(@direct_message.room)
+      @room = Room.find(@direct_message.room.id)
+  	  # redirect_to room_path(@direct_message.room)
   	else
   	  redirect_back(fallback_location: posts_path)
   	end
